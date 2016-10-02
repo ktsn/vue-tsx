15109061c54f47e3c32df8a43d409111f91b8c34
ktsn <ktsn55@gmail.com>
2016-10-02 11:53:20 +0900

    Add webpack config for example build

---
+++ example/main.tsx
@@ -0,0 +1,10 @@
+import Vue = require('vue')
+import App from './App'
+
+new Vue({
+  render (h) {
+    return (
+      <App name="Vue.js" />
+    )
+  }
+}).$mount('#app')
---
+++ example/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname),
+  entry: './main.tsx',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'build.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.ts', '.tsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'babel!ts' }
+    ]
+  }
+}
